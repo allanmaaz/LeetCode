d34819a738f6c23009321628f14c1c4cdfc6d008
allanmaaz <allanmaazimdad@gmail.com>
2025-07-03 19:10:08 +0530

    Time: 0 ms (100%), Space: 45.6 MB (77.12%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,20 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0,r=nums.length-1;
-        
-        while(l<=r){
-            
-                int mid=l+((r-l)/2);
-                if(nums[mid]>target){
-                    r=mid-1;
-                }else if(nums[mid]<target){
-                    l=mid+1;
-                }else{
-                  return mid;
-                }
-            
-        }
-        return -1;
-        
+int aa=Arrays.binarySearch(nums,target);
+return aa>=0?aa:-1;
     }
 }
