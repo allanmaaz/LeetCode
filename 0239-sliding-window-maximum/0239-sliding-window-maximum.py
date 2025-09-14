61f83341e68afd0a6a01cf1bf6c0dbd703589724
Mohammed Maaz <allanmaazimdad@gmail.com>
2025-09-14 23:49:13 +0530

    Time: 167 ms (58.07%), Space: 30.9 MB (86.66%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -3,21 +3,16 @@ from typing import List
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = deque()   # stores indices of elements in the current window
+        dq = deque()  
         res = []
 
         for i in range(len(nums)):
-            # Remove indices that are out of the window
             if dq and dq[0] <= i - k:
                 dq.popleft()
-
-            # Remove elements smaller than nums[i] from the back
             while dq and nums[dq[-1]] <= nums[i]:
                 dq.pop()
 
             dq.append(i)
-
-            # Append result when window size is at least k
             if i >= k - 1:
                 res.append(nums[dq[0]])
 
