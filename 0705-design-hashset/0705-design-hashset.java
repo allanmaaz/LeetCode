16998e8cccd87e16588ae3bd2569160b5b1bde85
Mohammed Maaz <allanmaazimdad@gmail.com>
2026-01-16 23:23:28 +0530

    Time: 12 ms (99.12%), Space: 52 MB (68.03%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,23 +1,15 @@
 class MyHashSet {
-
     // private  int SIZE = 1000001;
     private boolean[] buckets;
-
     public MyHashSet() {
        buckets = new boolean[1000001];
-    
     }
-
-    public void add(int key) {
-       
+    public void add(int key) {  
         buckets[key]=true;
     }
-
     public void remove(int key) {
          buckets[key]=false;
     }
-
     public boolean contains(int key) {
         return buckets[key];
-}
-}
+}}
