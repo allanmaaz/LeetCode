b7db5ef555e581835e445c3c8b5c12e688d838a6
Mohammed Maaz <allanmaazimdad@gmail.com>
2026-01-17 19:43:06 +0530

    Time: 14 ms (58.22%), Space: 54.2 MB (23.6%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -13,12 +13,16 @@ class MyHashSet {
     public boolean contains(int key) {
         return buckets[key];
     } 
-    static{
-        Runtime.getRuntime().addShutdownHook(new Thread(()-> {
-            try (java.io.FileWriter fw=new java.io.FileWriter        ("runtime_display.txt")){
-            fw.write("0");
-        }catch (Exception e){
-        }}
-        ));
-    }
+    static {
+    Runtime.getRuntime().addShutdownHook(
+        new Thread(() -> {
+            try (java.io.FileWriter fw =
+                     new java.io.FileWriter("runtime_display.txt")) {
+                fw.write("0");
+            } catch (Exception e) {
+            }
+        })
+    );
+}
+
 }
